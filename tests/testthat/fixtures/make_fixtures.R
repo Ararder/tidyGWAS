1754813a55cf71423a8aa0b6166b1efd09c35f8d
Arvid Harder <arvid.harder@gmail.com>
2023-08-01 19:57:50 +0200

    put rs_merge_arch in test fixtures

--- tests/testthat/fixtures/make_fixtures.R
+++ tests/testthat/fixtures/make_fixtures.R
@@ -1,3 +1,5 @@
+rs_merge_arch <- tidyGWAS::rs_merge_arch
+save(rs_merge_arch, file = test_path("fixtures/rs_merge_arch.rds"))
 
 b38 <- map_to_dbsnp(dplyr::tibble(test_file), by = "rsid", build = 38)
 b37 <- map_to_dbsnp(dplyr::tibble(test_file), by = "rsid", build = 37)
