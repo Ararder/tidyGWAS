44a3b0cd611544e2fc70654fdad27006fb705acb
Arvid Harder <arvid.harder@gmail.com>
2025-07-01 12:11:13 +0200

    bump

--- R/gwas-catalog.R
+++ R/gwas-catalog.R
@@ -1,8 +1,19 @@
 
+#' Download summary statistics from GWAS catalog
+#'
+#' @param study_id A single character string with study ID, e.g. "GCST90475332"
+#' @param quiet TRUE/FALSE - controls progress bar for downloading
+#'
+#' @returns a filepath to downloaded summary statistics
+#' @export
+#'
+#' @examples \dontrun{
+#' from_gwas_catalog("GCST90475332")
+#' }
 from_gwas_catalog <- function(study_id, quiet=FALSE) {
 
 
-  rlang::is_scalar_character(study_id) || cli::cli_abort("study_id must be a single character string")
+  rlang::is_scalar_character(study_id) || cli::cli_abort("study_id: {.arg {study_id}} must be a single character string")
   is_sumstats_available(study_id) || cli::cli_abort("Full summary statistics for {study_id} must be available in GWAS catalog")
   workdir <- fs::dir_create(fs::path(tempdir(), study_id))
 
@@ -19,8 +30,8 @@ from_gwas_catalog <- function(study_id, quiet=FALSE) {
   meta <- yaml::read_yaml(meta_file)
 
 
-  cli::cli_alert_info("Downloading GWAS summary statistics → {.path {gwas_file}}")
-  curl::curl_download(url = base_gwas,destfile = gwas_file, quiet=quiet)
+  cli::cli_alert_info("Downloading GWAS summary statistics {.path {gwas_file}}")
+  curl::curl_download(url = base_gwas, destfile = gwas_file, quiet=quiet)
 
   gwas_file
 }
